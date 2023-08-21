069dac8ad6780a3d28c7e7f794071d5673f105ec
Mateus Oliveira Milane <mateus.milane555@gmail.com>
2023-08-20 23:09:50 -0300

    Inicio implementacao UI

--- src/main/java/br/com/Main.java
+++ src/main/java/br/com/Main.java
@@ -1,7 +1,42 @@
 package br.com;
 
+import br.com.enums.TipoContato;
+import br.com.model.Contato;
+import br.com.ui.BasicUI;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        BasicUI ui = new BasicUI("Agenda de Contatos");
+        ui.show();
+
+//        Agenda agenda = new Agenda();
+//        for (int i = 0; i < 10; i++) {
+//            Contato contato = new Contato();
+//            contato.setTipoContato(TipoContato.PESSOAL);
+//            contato.setNome("Nome " + i);
+//            contato.setSobrenome("Teste");
+//            agenda.adicionarContato(contato);
+//        }
+//
+//        System.out.println("Pagina 1");
+//        List<Contato> pagina1 = agenda.listarContatos(1, 4);
+//        for (int i = 0; i < pagina1.size(); i++) {
+//            System.out.println(pagina1.get(i).getNomeCompleto());
+//        }
+//
+//        System.out.println("Pagina 2");
+//        List<Contato> pagina2 = agenda.listarContatos(2, 4);
+//        for (int i = 0; i < pagina2.size(); i++) {
+//            System.out.println(pagina2.get(i).getNomeCompleto());
+//        }
+//
+//        System.out.println("Pagina 3");
+//        List<Contato> pagina3 = agenda.listarContatos(3, 4);
+//        for (int i = 0; i < pagina3.size(); i++) {
+//            System.out.println(pagina3.get(i).getNomeCompleto());
+//        }
     }
 }
---
+++ src/main/java/br/com/ui/PagedListUI.java
@@ -0,0 +1,30 @@
+package br.com.ui;
+
+import br.com.util.ConsoleUIHelper;
+
+public class PagedListUI extends BasicUI {
+    public PagedListUI(String titulo) {
+        super(titulo);
+    }
+
+    public PagedListUI(int colunas, int linhas, String titulo) {
+        super(colunas, linhas, titulo);
+    }
+
+    @Override
+    public int drawContent() {
+        this.colunas = 10;
+        return 0;
+    }
+
+    @Override
+    public int menuLines() {
+
+        return 0;
+    }
+
+    @Override
+    public boolean drawMenu() {
+        return ConsoleUIHelper.askConfirm("Sair do sistema?", "Não", "Sim");
+    }
+}
